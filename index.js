48a9e250f81c7c299463c5d41dd603a1ee35dbde
Alejandro Ruiz <alejandroruizponce93@gmail.com>
2017-03-20 20:49:59 +0000

    Nueva version

--- index.js
+++ index.js
@@ -4,22 +4,40 @@ var app = express();
 app.use(bodyParser.json());
 
 var path = require('path');
+var dataStore = require('nedb');
+var dbFileName = path.join(__dirname,'contacts.json');
+
+var db = new dataStore({
+    filename : dbFileName,
+    autoload : true
+});
 
 var port = (process.env.PORT || 3000);
 
 var baseAPI = "/api/v1";
 
-var contacts = [{
-    name: "pepe",
-    phone: "12345",
-    email: "pepe@pepe.com"
-}, {
-    name: "luis",
-    phone: "67890",
-    email: "luis@luis.com"
-}];
 
 
+app.get(baseAPI+"/contacts/", (request, response) => {
+    
+    var contacts;
+    
+    db.find({},(err, contacts) => {
+        response.send(contacts);
+    });
+    
+    console.log("GET /contacts");
+});
+
+app.post(baseAPI+"/contacts/", (request, response) => {
+    
+    var contact = request.body;
+    
+    db.insert(contact);
+    response.sendStatus(201);
+    
+    console.log("POST /contacts");
+});
 
 app.get(baseAPI+"/contacts/:name", (request, response) => {
     
