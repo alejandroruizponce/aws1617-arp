1f77afbc2986b7245c5566c71c6561824f7c61c8
Alejandro Ruiz <alejandroruizponce93@gmail.com>
2017-03-20 21:01:12 +0000

    Nueva version

--- index.js
+++ index.js
@@ -7,15 +7,60 @@ var path = require('path');
 var dataStore = require('nedb');
 var dbFileName = path.join(__dirname,'contacts.json');
 
-var db = new dataStore({
-    filename : dbFileName,
-    autoload : true
-});
+
 
 var port = (process.env.PORT || 3000);
 
 var baseAPI = "/api/v1";
 
+var contacts = [{
+    name: "pepe",
+    phone: "12345",
+    email: "pepe@pepe.com"
+}, {
+    name: "luis",
+    phone: "67890",
+    email: "luis@luis.com"
+}];
+
+
+
+app.get(baseAPI+"/contacts/:name", (request, response) => {
+    
+    var name = request.params.name;
+    
+    var contact = contacts.filter((contact) => {
+        return (contact.name == name);
+    })[0];
+
+    
+    response.send(contacts);
+    console.log("GET /contacts");
+});
+
+app.delete(baseAPI+"/contacts", (request, response) => {
+    
+    var contact = request.body;
+    contacts = [];
+    
+    response.sendStatus(200);
+    
+    console.log("DELETE /contacts");
+});
+
+
+
+app.put(baseAPI+"/contacts/:name", (request, response) => {
+    var name = request.params.name;
+    var updatedContact = request.body;
+    
+    contacts = contacts.map((contact) => {
+        if(contact.name == name)
+            return updatedContact;
+        else
+            return contact;
+    })
+})
 
 
 app.listen(port, () => {
