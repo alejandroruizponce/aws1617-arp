b15027d2e963af8a26e201c576e704fa2d083f3d
Alejandro Ruiz <alejandroruizponce93@gmail.com>
2017-03-20 20:58:27 +0000

    Nueva version

--- index.js
+++ index.js
@@ -18,28 +18,6 @@ var baseAPI = "/api/v1";
 
 
 
-app.get(baseAPI+"/contacts/", (request, response) => {
-    
-    
-    db.find({},(err, contacts) => {
-        response.send(contacts);
-    });
-    
-    console.log("GET /contacts");
-});
-
-app.post(baseAPI+"/contacts/", (request, response) => {
-    
-    var contact = request.body;
-    
-    db.insert(contact);
-    response.sendStatus(201);
-    
-    console.log("POST /contacts");
-});
-
-
-
 app.listen(port, () => {
     console.log("Server up and running!!");
 });
